8fad785f66adc6b8487bde124882fda6c4a1d611
Craftzman7 <zezetta@icloud.com>
2021-06-04 21:34:37 +0000

    pog

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require(`express`)
 var app = new express()
 var fs = require("fs");
 var routePath="./routes/"; 
+app.use(express.static, "images")
 fs.readdirSync(routePath).forEach(function(file) {
     var route = routePath+file;
     require(route)(app);
 });
-app.listen(3000)
+app.listen(80)
--- routes/hello.js
+++ routes/hello.js
@@ -1,5 +1,5 @@
 module.exports=function(app){
-    app.get('/hello',function(req,res){
-       res.json({hello: `world`});
+    app.get('/api/hello',function(req,res){
+       res.json({hello: `world`, status: `200`});
     })
 }
